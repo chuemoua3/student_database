72b24b1e01ab11eee663b320ab4589bfc2ba2347
chue moua <chuemoua12@outlook.com>
2020-05-22 16:48:51 -0400

    first commit

---
+++ StudentMain.java
@@ -0,0 +1,12 @@
+public class StudentMain {
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.enroll();
+        student1.payTuition();
+        System.out.println(student1.toString());
+        //Ask how many new users we want to add
+
+
+        //create n number of new students
+    }
+}
